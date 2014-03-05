21fdc2951d73f57247da4330719064514353cd95
Adamska <geoffrey.heckmann@epitech.eu>
2014-03-05 13:23:06 +0100

    lib

---
+++ libgdl_gl-2012.4/main.cpp
@@ -0,0 +1,25 @@
+#include "include/Clock.hpp"
+#include "include/Color.hpp"
+#include "include/Game.hpp"
+#include "include/GameClock.hpp"
+#include "include/Image.hpp"
+#include "include/Input.hpp"
+#include "include/Model.hpp"
+#include "include/Window.hpp"
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+////////////////////////////////////////////////////////////////////////////////
+/// Declaration de la fonction main
+////////////////////////////////////////////////////////////////////////////////
+
+#include <cstdlib>
+//#include "MyGame.hpp"
+
+int main(void)
+{
+  // MyGame game;
+  // game.run();
+  return EXIT_SUCCESS;
+}
